879bb0076872a8010e1b1b906ae6529a56a1d0f9
Kinshuk Bairagi <me@kinshuk.in>
2019-08-27 13:31:33 +0530

    build using devcontainer

---
+++ make-deb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make
+mkdir -p /usr/lib/freeswitch/mod/
+make install
+
+checkinstall \
+    --pkgsource="https://github.com/voiceip/mod_bcg729" \
+    --pkglicense="Mozilla Public License (MPL)" \
+    --deldesc=no \
+    --nodoc \
+    --maintainer="$USER\\<$USER@$HOSTNAME\\>" \
+    --pkgarch=$(dpkg --print-architecture) \
+    --pkgname=freeswitch-mod-bcg729  \
+    --requires="freeswitch \(\>= 1.8\)" \
